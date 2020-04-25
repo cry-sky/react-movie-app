89b2aba37c47afc2c27320df64618966a9c92e39
cry-sky <9deletedata@gmail.com>
2020-04-25 14:24:31 +0300

    Updates

---
+++ precache-manifest.0ea1da512fcc6c62881f5dd6a3656d70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62dbdd4159b4b0d21e4e2b2a58c1cb8",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "22070a031d4e56c9c3d6",
+    "url": "/react-movie-app/static/css/main.f2a94455.chunk.css"
+  },
+  {
+    "revision": "40c7c8d5cea4507b73c6",
+    "url": "/react-movie-app/static/js/2.19ff9cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app/static/js/2.19ff9cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22070a031d4e56c9c3d6",
+    "url": "/react-movie-app/static/js/main.4a529135.chunk.js"
+  },
+  {
+    "revision": "4965203439e11a5b0984",
+    "url": "/react-movie-app/static/js/runtime-main.085f0b40.js"
+  }
+]);
